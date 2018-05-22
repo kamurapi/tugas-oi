b8f46c11cddc0c5ab726264c4654a024fec79020
Dimas Kamurapi <32903333+kamurapi@users.noreply.github.com>
2018-05-22 23:06:38 +0700

    buat scan per line nya

--- codenya.cpp
+++ codenya.cpp
@@ -1,4 +1,22 @@
-int main 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
 {
-  printf("yuk semangat");
+    string line;
+    int numberline;
+    numberline=1;
+    ifstream myfile ("lexicalinput.txt");
+    if (!myfile.is_open()){
+        printf("Unable to open file");
+        exit(0);
+    }
+    else {
+        while ( getline (myfile,line) ){
+            cout << line;
+            printf("   [line : %d]\n",numberline);
+            numberline++;
+        }
+    }
+    return 0;
 }
